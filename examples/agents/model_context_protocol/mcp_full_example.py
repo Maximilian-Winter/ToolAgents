08650dd5470d1b90a4f173be32ef398977cd3433
Maximilian Winter <maximilian.winter.91@gmail.com>
2025-02-27 19:42:54 +0100

    Update mcp_full_example.py

--- examples/agents/model_context_protocol/mcp_full_example.py
+++ examples/agents/model_context_protocol/mcp_full_example.py
@@ -9,7 +9,7 @@ from ToolAgents.agents import ChatToolAgent
 from ToolAgents.messages import ChatHistory
 from ToolAgents.messages.chat_message import ChatMessage
 from ToolAgents.provider import OpenAIChatAPI
-from ToolAgents.mcp_server import MCPServer, MCPServerConfig, create_and_run_mcp_server
+from ToolAgents.utilities.mcp_server import MCPServer, MCPServerConfig, create_and_run_mcp_server
 from ToolAgents.mcp_tool import MCPToolRegistry, MCPTool, MCPToolDefinition
 
 from pydantic import BaseModel, Field
