d60516bb210ed67bd9bb927b772440cf9d1749ae
Maximilian Winter <maximilian.winter.91@gmail.com>
2025-08-17 00:42:09 +0200

    Revert "Update __init__.py"
    This reverts commit 94157a66403f47456a210380a93c93dccf770c8d.

--- src/ToolAgents/knowledge/vector_database/__init__.py
+++ src/ToolAgents/knowledge/vector_database/__init__.py
@@ -1,4 +1,4 @@
 from .rag import RAG
 from .reranking_provider import RerankingProvider, RerankingResult, RerankedDocument
-from .embedding_provider import EmbeddingProvider, EmbeddingResult, PrefixConfig
-from .vector_database_provider import VectorDatabaseProvider, VectorSearchResult, DocumentEntry, CollectionInfo
+from .embedding_provider import EmbeddingProvider, EmbeddingResult
+from .vector_database_provider import VectorDatabaseProvider, VectorSearchResult
